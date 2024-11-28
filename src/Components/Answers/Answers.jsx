49db0ffb9ca2a17d559b840c62468609663426ee
Omar Mohamed <omar.mohamed.eltabakh1@gmail.com>
2024-11-28 23:11:49 +0200

    Update

--- src/Components/Answers/Answers.jsx
+++ src/Components/Answers/Answers.jsx
@@ -50,7 +50,8 @@ export default function Answers() {
                     <div className={`${style.answerMenu} `}>
                         <h2>{`${gender === "male" ? "عزيزي" : "عزيزتي"}`} {userName}، احتمالية إصابتك {`${diseaseType === "joint" ? "بـ" : "بمرض"}`} {diseaseType === "diabetes" ? "السكري" : diseaseType === "bloodPressure" ? "الضغط" : diseaseType === "cardiovascular" ? "القلب" : diseaseType === "joint" ? "كسور في العظام" : diseaseType === "immunology" ? " مناعي " : "غير محدد"}{riskPercentage < 30 ? " منخفضة" : riskPercentage < 50 ? " متوسطة" : " مرتفعة"} ({`${riskPercentage}%`})
                             .</h2>
-                        <p>يرجى اتباع النصائح التالية للحفاظ على صحتك:</p>
+                        {riskPercentage > 50 && <p className='m-0 text-center  alert alert-danger py-1 mb-2 '>يرجي التوجه الي اقرب وحده صحية التابعه لمدينة {address}</p>}
+                        <p>  ايضا يرجى اتباع النصائح التالية للحفاظ على صحتك  :</p>
                         <ul className='pe-4'>
                             {guidelines.map((guideline, index) => (
                                 <li key={index}>{guideline}</li>
